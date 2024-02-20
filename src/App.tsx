0671807030197fda4feb8b1d14cf14004380831e
julia-lahtina <j.lakhtin@gmail.com>
2024-02-20 19:30:45 +0100

    add new attribute to Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -35,14 +35,16 @@ function App() {
         setTasks(nextState)
     }
 
-    function changeTaskStatus(id: string) {  //"2"
+    function changeTaskStatus(id: string, newIsDone: boolean) {  //"2"
         /*const task = tasks.find(t => t.id === id) //undefined если нет совпадений
         if (task) {
             task.isDone = !task.isDone  // меняем статус мутабельно, то есть мы напрямую залезли в таску и поменяли ее
             setTasks([...tasks]) // потом все засунули в новый массив, реакт не увидит, где именно произошли изменения, т.е. он будет знать, что изменения были, но не будет знать, где конкретно и поэтому перерисует весь массив, а не конкретно одну таску, где были изменения
         }*/  // -------------------- это вариант, как делать не надо ------------------
 
-        const nextState = tasks.map(t => t.id === id ? {...t, isDone: !t.isDone} : t) //здесь реакт увидит копию конкретной таски, это значит для реакта, что там произошли изменения и он перерисует только эту таску
+        const nextState = tasks.map(t => t.id === id
+            ? {...t, isDone: newIsDone}
+            : t) //здесь реакт увидит копию конкретной таски, это значит для реакта, что там произошли изменения и он перерисует только эту таску
         setTasks(nextState)
     }
 
@@ -67,6 +69,7 @@ function App() {
         <div className="App">
             <Todolist title="What to learn"
                       tasks={tasksForTodolist}
+                      filter={filter}
                       removeTask={removeTask}
                       changeFilter={changeFilter}
                       addTask={addTask}
