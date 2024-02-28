a994bbff454af4fe629c02973560892a7458f57b
julia-lahtina <j.lakhtin@gmail.com>
2024-02-28 21:25:32 +0100

    sprint2 lesson1

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,96 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TaskType, Todolist} from './Todolist';
+import {Todolist} from './Todolist';
 import {v1} from 'uuid';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 
-export function getFilteredTasks(allTasks: Array<TaskType>, filterValue: FilterValuesType): Array<TaskType> { //последний :Array<TaskType> это то, что возвращает функция, т.е. типизация return
-    let tasksForTodolist = allTasks;
-    if (filterValue === 'active') {
-        tasksForTodolist = allTasks.filter(t => !t.isDone);
-    }
-    if (filterValue === 'completed') {
-        tasksForTodolist = allTasks.filter(t => t.isDone);
-    }
-    return tasksForTodolist
+type TodolistsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
 }
 
-
 function App() {
-    console.log(v1())
 
-    // BLL
-    let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-        {id: v1(), title: 'Rest API', isDone: false},
-        {id: v1(), title: 'GraphQL', isDone: false},
-    ]);
+    let todolistID1 = v1();
+    let todolistID2 = v1();
 
-    let [filter, setFilter] = useState<FilterValuesType>('all');
+    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
+    ])
 
-    // CRUD tasks
-    function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id !== id);
+    let [tasks, setTasks] = useState({
+        [todolistID1]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
+            {id: v1(), title: 'Rest API', isDone: false},
+            {id: v1(), title: 'GraphQL', isDone: false},
+        ],
+        [todolistID2]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
+            {id: v1(), title: 'Rest API', isDone: false},
+            {id: v1(), title: 'GraphQL', isDone: false},
+        ],
+    });
+
+
+    function removeTask(taskId: string) {
+        let filteredTasks = tasks.filter(t => t.id !== taskId);
         setTasks(filteredTasks);
     }
 
-    function addTask(taskTitle: string) {   //от пользователя получаем данные, это какое-то название таски, то есть строка
-        const newTask: TaskType = {
-            id: v1(),
-            title: taskTitle,
-            isDone: false
-        }
-        const nextState = [newTask, ...tasks]
-        setTasks(nextState)
+    function addTask(title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
     }
 
-    function changeTaskStatus(id: string, newIsDone: boolean) {  //"2"
-        /*const task = tasks.find(t => t.id === id) //undefined если нет совпадений
+    function changeStatus(taskId: string, isDone: boolean) {
+        let task = tasks.find(t => t.id === taskId);
         if (task) {
-            task.isDone = !task.isDone  // меняем статус мутабельно, то есть мы напрямую залезли в таску и поменяли ее
-            setTasks([...tasks]) // потом все засунули в новый массив, реакт не увидит, где именно произошли изменения, т.е. он будет знать, что изменения были, но не будет знать, где конкретно и поэтому перерисует весь массив, а не конкретно одну таску, где были изменения
-        }*/  // -------------------- это вариант, как делать не надо ------------------
-
-        const nextState = tasks.map(t => t.id === id
-            ? {...t, isDone: newIsDone}
-            : t) //здесь реакт увидит копию конкретной таски, это значит для реакта, что там произошли изменения и он перерисует только эту таску
-        setTasks(nextState)
-    }
-
-
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value);
-    }
-
-
-    //UI
-
-    /*    let tasksForTodolist = tasks;
-        if (filter === 'active') {
-            tasksForTodolist = tasks.filter(t => !t.isDone);
+            task.isDone = isDone;
         }
-        if (filter === 'completed') {
-            tasksForTodolist = tasks.filter(t => t.isDone);
-        }*/
 
+        setTasks([...tasks]);
+    }
 
-    const filteredTasks = getFilteredTasks(tasks, filter)
+    function changeFilter(todolistID: string, value: FilterValuesType) {
+        setTodolists(todolists.map(el => el.id === todolistID ? {...el, filter: value} : el))
+    }
+
 
     return (
         <div className="App">
-            <Todolist title="What to learn"
-                      tasks={filteredTasks}
-                      filter={filter}
-                      removeTask={removeTask}
-                      changeFilter={changeFilter}
-                      addTask={addTask}
-                      changeTaskStatus={changeTaskStatus}
-            />
+            {todolists.map(el => {
+
+                let tasksForTodolist = tasks[el.id];
+                if (el.filter === 'active') {
+                    tasksForTodolist = tasks[el.id].filter(t => !t.isDone);
+                }
+                if (el.filter === 'completed') {
+                    tasksForTodolist = tasks[el.id].filter(t => t.isDone);
+                }
+                return (
+                    <Todolist
+                        key={el.id}
+                        todolistID={el.id}
+                        title={el.title}
+                        tasks={tasksForTodolist}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeStatus}
+                        filter={el.filter}
+                    />
+                )
+            })}
         </div>
     );
-
 }
 
 export default App;
