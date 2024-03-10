cf674c10e185a476f0c363ad2f27b4e3e91b382d
julia-lahtina <j.lakhtin@gmail.com>
2024-03-10 15:11:10 +0100

    change component, add new state

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -1,11 +1,21 @@
-// @flow 
 import * as React from 'react';
+import {useState} from 'react';
 
 type Props = {
     title: string
 };
 export const EditableSpan = ({title}: Props) => {
+
+    const [edit, setEdit] = useState(false)
+
+    const editHandler = () => {
+        setEdit(!edit)
+    }
+
     return (
-        <span>{title}</span>
+        edit
+            ? <input type="text" value={title} onBlur={editHandler} autoFocus/>
+            : <span onDoubleClick={editHandler}>{title}</span>
+
     );
 };
