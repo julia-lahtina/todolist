da10464e0f09bd0e24b2f8e73d6ec18cdf5bb473
julia-lahtina <j.lakhtin@gmail.com>
2024-03-31 17:53:00 +0200

    sprint 3 lesson 1

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -5,7 +5,6 @@ import {EditableSpan} from './EditableSpan';
 import IconButton from '@mui/material/IconButton/IconButton';
 import {Delete} from "@mui/icons-material";
 import {Button, Checkbox} from "@mui/material";
-import {CheckBox} from './components/checkbox/CheckBox';
 
 
 export type TaskType = {
@@ -44,11 +43,6 @@ export function Todolist(props: PropsType) {
     const onActiveClickHandler = () => props.changeFilter("active", props.id);
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
 
-    const changeStatusHandler = (taskId: string, isDone: boolean) => {
-        props.changeTaskStatus(taskId, isDone, props.id)
-    }
-
-
     return <div>
         <h3> <EditableSpan value={props.title} onChange={changeTodolistTitle} />
             <IconButton onClick={removeTodolist}>
@@ -60,14 +54,21 @@ export function Todolist(props: PropsType) {
             {
                 props.tasks.map(t => {
                     const onClickHandler = () => props.removeTask(t.id, props.id)
-
+                    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+                        let newIsDoneValue = e.currentTarget.checked;
+                        props.changeTaskStatus(t.id, newIsDoneValue, props.id);
+                    }
                     const onTitleChangeHandler = (newValue: string) => {
                         props.changeTaskTitle(t.id, newValue, props.id);
                     }
 
 
                     return <div key={t.id} className={t.isDone ? "is-done" : ""}>
-                        <CheckBox checked={t.isDone} color={'primary'} onChange={(newIsDoneValue) => changeStatusHandler(t.id, newIsDoneValue)}/>
+                        <Checkbox
+                            checked={t.isDone}
+                            color="primary"
+                            onChange={onChangeHandler}
+                        />
 
                         <EditableSpan value={t.title} onChange={onTitleChangeHandler} />
                         <IconButton onClick={onClickHandler}>
---
+++ src/state/tasks-reducer.ts
@@ -0,0 +1,96 @@
+import {TasksStateType} from '../App';
+import {v1} from 'uuid';
+import {AddTodolistActionType, RemoveTodolistActionType} from './todolists-reducer';
+
+
+export type FirstActionType = {
+    type: 'REMOVE-TASK',
+    id: string
+    todolistId: string
+}
+
+export type SecondActionType = {
+    type: 'ADD-TASK'
+    title: string
+    todolistId: string
+}
+
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS'
+    taskId: string
+    isDone: boolean
+    todolistId: string
+}
+
+export type ChangeTaskTitleActionType = {
+    type: 'CHANGE-TASK-TITLE'
+    taskId: string
+    taskTitle: string
+    todolistId: string
+}
+
+
+type ActionsType = FirstActionType
+    | SecondActionType
+    | ChangeTaskStatusActionType
+    | ChangeTaskTitleActionType
+    | AddTodolistActionType
+    | RemoveTodolistActionType
+
+export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
+    switch (action.type) {
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.id)
+            }
+        case 'ADD-TASK':
+            let newTask = {id: v1(), title: action.title, isDone: false}
+            return {
+                ...state,
+                [action.todolistId]: [newTask, ...state[action.todolistId]]
+            }
+        case 'CHANGE-TASK-STATUS':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId]
+                    .map(t => t.id === action.taskId ? {...t, isDone: action.isDone} : t)
+            }
+        case 'CHANGE-TASK-TITLE':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId]
+                    .map(t => t.id === action.taskId ? {...t, title: action.taskTitle} : t)
+            }
+        case 'ADD-TODOLIST':
+            return {
+                ...state,
+                [action.todolistId]: []
+            }
+        case 'REMOVE-TODOLIST': {
+            let copyState = {...state}
+            delete copyState[action.id]
+            return copyState
+        }
+
+        default:
+            throw new Error('I don\'t understand this type')
+    }
+}
+
+export const removeTaskAC = (id: string, todolistId: string): FirstActionType => {
+    return {type: 'REMOVE-TASK', id, todolistId} as const
+}
+
+export const addTaskAC = (title: string, todolistId: string): SecondActionType => {
+    return {type: 'ADD-TASK', title, todolistId} as const
+}
+
+export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string): ChangeTaskStatusActionType => {
+    return {type: 'CHANGE-TASK-STATUS', taskId, isDone, todolistId} as const
+}
+
+export const changeTaskTitleAC = (taskId: string, taskTitle: string, todolistId: string): ChangeTaskTitleActionType => {
+    return {type: 'CHANGE-TASK-TITLE', taskId, taskTitle, todolistId} as const
+}
+
