cc9415ad967beb976809a7f701f6d8307fdbeae6
julia-lahtina <j.lakhtin@gmail.com>
2024-04-14 17:54:39 +0200

    sprint 3 lesson 3

--- src/TodolistWithRedux.tsx
+++ src/TodolistWithRedux.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useCallback} from 'react';
+import React, {ChangeEvent, memo, useCallback} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
@@ -23,24 +23,28 @@ type PropsType = {
     filter: FilterValuesType
 }
 
-export function TodolistWithRedux(props: PropsType) {
+export const TodolistWithRedux = memo((props: PropsType) => {
+
+    console.log('Todolist')
 
     let tasks = useSelector<AppRootStateType, Array<TaskType>>(state => state.tasks[props.id])
     const dispatch = useDispatch()
+
+
     const addTask = useCallback((title: string) => {
             dispatch(addTaskAC(title, props.id));
         }
-    , [dispatch, props.id])
-    const removeTodolist = () => {
+        , [dispatch, props.id])
+    const removeTodolist = useCallback(() => {
         dispatch(RemoveTodolistAC(props.id));
-    }
-    const changeTodolistTitle = (title: string) => {
+    }, [dispatch, props.id])
+    const changeTodolistTitle = useCallback((title: string) => {
         dispatch(ChangeTodolistTitleAC(props.id, title));
-    }
+    }, [dispatch, props.id])
 
-    const onAllClickHandler = () => dispatch(ChangeTodolistFilterAC(props.id, "all"));
-    const onActiveClickHandler = () => dispatch(ChangeTodolistFilterAC(props.id, "active"));
-    const onCompletedClickHandler = () => dispatch(ChangeTodolistFilterAC(props.id, "completed"));
+    const onAllClickHandler = useCallback(() => dispatch(ChangeTodolistFilterAC(props.id, "all")), [dispatch, props.id])
+    const onActiveClickHandler = useCallback(() => dispatch(ChangeTodolistFilterAC(props.id, "active")), [dispatch, props.id])
+    const onCompletedClickHandler = useCallback(() => dispatch(ChangeTodolistFilterAC(props.id, "completed")), [dispatch, props.id])
 
 
 
@@ -102,6 +106,6 @@ export function TodolistWithRedux(props: PropsType) {
             </Button>
         </div>
     </div>
-}
+})
 
 
