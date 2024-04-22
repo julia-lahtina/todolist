a7acebb43aab3d5372203bca8c28fd2ccdbc8af9
julia-lahtina <j.lakhtin@gmail.com>
2024-04-22 21:16:27 +0200

    sprint 3 lesson 4 - refactoring with decorators

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -2,6 +2,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 import AppWithRedux from '../AppWithRedux';
 import {Provider} from 'react-redux';
 import {store} from '../state/store';
+import {ReduxStoreProviderDecorator} from '../state/ReduxStoreProviderDecorator';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof AppWithRedux> = {
@@ -16,18 +17,14 @@ const meta: Meta<typeof AppWithRedux> = {
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
 
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-
+    decorators: [ReduxStoreProviderDecorator]
 }
 
 export default meta;
 type Story = StoryObj<typeof AppWithRedux>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const AppWithReduxStory: Story = {
-    render: () => {
-        return <Provider store={store}><AppWithRedux/></Provider>
-    }
-};
+export const AppWithReduxStory: Story = {};
 
 
 
